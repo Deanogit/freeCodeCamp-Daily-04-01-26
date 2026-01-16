6186037097d4c74a0c299023b4c8e5f0fcb26dc0
Dean Howe <hideanhowe@gmail.com>
2026-01-16 12:19:03 +0000

    testing if

--- index.js
+++ index.js
@@ -6,3 +6,18 @@
 // The year is evenly divisible by 4, and
 // The year is not evenly divisible by 100, unless
 // The year is evenly divisible by 400.
+
+function isLeapYear(year) {
+  console.log(year);
+
+  // the year is divisible by 4
+  return (
+    year % 4 === 0 &&
+    // the year is not divisible by 100,
+    year % 100 !== 0 &&
+    // unless the year is divisible by 400
+    year % 400 === 0
+  );
+
+  // return year;
+}
